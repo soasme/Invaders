c1d4dbc64743c3ffb4f7dda1b1cfc9eff2f7741a
soasme <soasme@gmail.com>
2020-01-25 21:54:59 +1300

    init.

---
+++ game/enemy/enemymgr.go
@@ -0,0 +1,69 @@
+components {
+  id: "enemymgr"
+  component: "/game/enemy/enemymgr.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "enemyfactory"
+  type: "factory"
+  data: "prototype: \"/game/enemy/enemy.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "enemy2factory"
+  type: "factory"
+  data: "prototype: \"/game/enemy/enemy2.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "enemy3factory"
+  type: "factory"
+  data: "prototype: \"/game/enemy/enemy3.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
